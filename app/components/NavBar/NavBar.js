ef74e07b705808d414cb83064a50b0e74d08a3a5
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-05-25 09:07:32 -0700

    navbar styling

--- app/components/NavBar/NavBar.js
+++ app/components/NavBar/NavBar.js
@@ -1,8 +1,13 @@
+import Link from 'next/link'
 import './NavBar.css'
 
 const NavBar = () => {
     return (
-        <div className='nav'>NavBar</div>
+        <div className='nav'>
+            <Link href='#about' className='link'>About</Link>
+            <Link href='#about' className='link'>Projects</Link>
+            <Link href='#about' className='link'>Contact</Link>
+        </div>
     )
 }
 
