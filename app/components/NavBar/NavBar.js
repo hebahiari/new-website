efdc820ee3e2b05112fa17123081bb920cdd132e
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-05-28 09:31:00 -0700

    style updates

--- app/components/NavBar/NavBar.js
+++ app/components/NavBar/NavBar.js
@@ -4,9 +4,13 @@ import './NavBar.css'
 const NavBar = () => {
     return (
         <div className='nav'>
-            <Link href='#about' className='link'>About</Link>
-            <Link href='#about' className='link'>Projects</Link>
-            <Link href='#about' className='link'>Contact</Link>
+            <h1>Heba Alhyari</h1>
+            <p>Full Stack Engineer</p>
+            <div className='nav-links'>
+                <Link href='#about' className='link'>About</Link>
+                <Link href='#about' className='link'>Projects</Link>
+                <Link href='#about' className='link'>Contact</Link>
+            </div>
         </div>
     )
 }
